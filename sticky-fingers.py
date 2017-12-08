677d55997e102f7b61bce25ab414f78936c257ee
Max Granat <max.granat@gmail.com>
2017-12-07 23:09:31 -0600

    Add automated testing

--- sticky-fingers.py
+++ sticky-fingers.py
@@ -9,39 +9,13 @@ import minutiae
 import os
 import profile
 import time
+import sys
+import random
 import pdb
 
-def main2():
-  path = "Max\\School\\sticky-fingers\\figs\\png_txt\\figs_0"
-  directory = os.path.join("c:\\", path)
-  for root, dirs, files in os.walk(directory):
-    for file in files:
-      if file.endswith(".png"):
-        img = cv2.imread(os.path.join(root, file), 0)
-        enhanced, segmented = imageProcessing.enhance(img)
-
-        start = time.time()
-        min_img, coords = minutiae.extract(enhanced, segmented)
-        end = time.time()
-        print("Minutiae Extraction: " + str(end - start))
-
-        min_img = minutiae.visualize(img, coords)
-        # equalized = cv2.equalizeHist(img)
-        # gabor = imageProcessing.gabor(equalized)
-
-        # out = np.hstack((enhanced, min_img))
-        # out = np.hstack((equalized, gabor))
-        imageProcessing.display(min_img)
-        # imageProcessing.displayBinary(out)
-        # return
-
-def main():
-  path = "Max\\School\\sticky-fingers\\figs\\png_txt\\figs_0"
-  directory = os.path.join("c:\\", path)
-  name = "f0019_10.png"
-  img = cv2.imread(os.path.join(directory, name), 0)
-  name2 = "s0248_02.png"
-  img2 = cv2.imread(os.path.join(directory, name2), 0)
+def match(first, second):
+  img = cv2.imread(first, 0)
+  img2 = cv2.imread(second, 0)
 
   start = time.time()
   enhanced, segmented = imageProcessing.enhance(img)
@@ -62,5 +36,65 @@ def main():
   print("Minutiae Matching: " + str(end - start))
 
   print("Score: " + str(score / maxScore))
+
+  return score / maxScore
+
+def test(numTrials):
+  allImgs = ".\\figs\\png_txt\\"
+  imgDirs = os.listdir(allImgs)
+  firstFiles = []
+  secondFiles = []
+  for d in imgDirs:
+    imgDir = os.path.join(allImgs, d)
+    imgFiles = [os.path.join(imgDir, f) \
+    for f in os.listdir(imgDir) \
+    if f.endswith(".png") and f.startswith("s")]
+    secondFiles.extend(imgFiles)
+    imgFiles = [os.path.join(imgDir, f) \
+    for f in os.listdir(imgDir) \
+    if f.endswith(".png") and f.startswith("f")]
+    firstFiles.extend(imgFiles)
+
+  correctResults = []
+
+  # Correct trials
+  for i in range(numTrials):
+    ind = int(random.random() * len(firstFiles))
+    first = firstFiles[ind]
+    second = secondFiles[ind]
+    
+    correctResults.append(match(first, second))
+
+  incorrectResults = []
+
+  # Incorrect trials
+  for i in range(numTrials):
+    firstInd = int(random.random() * len(firstFiles))
+    secondInd = int(random.random() * len(secondFiles))
+    first = firstFiles[firstInd]
+    second = secondFiles[secondInd]
+    
+    incorrectResults.append(match(first, second))
+
+  print("Final Results\n")
+  print("Avg Correct Score: " + str(np.average(correctResults)))
+  print("Correct variance: " + str(np.var(correctResults)))
+  print("Avg Incorrect Score: " + str(np.average(incorrectResults)))
+  print("Incorrect variance: " + str(np.var(incorrectResults)))
+
+def main():
+  if len(sys.argv) < 2:
+    return
+
+  if sys.argv[1] == 'test':
+    if len(sys.argv) < 3:
+      return
+
+    numTrials = int(sys.argv[2])
+    test(numTrials)
+  elif sys.argv[1] == 'extract':
+    extractStats()
+  elif sys.argv[1] == 'cluster':
+    cluster()
 
 main()
